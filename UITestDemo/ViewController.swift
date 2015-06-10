5ccd534d85550829feedcb8de6c3c8eedd7bd5e4
eneko <eneko.alonso@gmail.com>
2015-06-09 21:39:23 -0700

    Initial Commit

---
+++ UITestDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITestDemo
+//
+//  Created by Eneko Alonso on 6/9/15.
+//  Copyright © 2015 Hathway, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
